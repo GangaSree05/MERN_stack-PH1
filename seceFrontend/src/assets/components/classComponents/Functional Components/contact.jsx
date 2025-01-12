89097c9e0f66efdb40962b91e2af19f040185668
GangaSree <gangasree05@gmail.com>
2025-01-12 23:28:26 +0530

    Adding react files

---
+++ seceFrontend/src/assets/components/classComponents/Functional Components/contact.jsx
@@ -0,0 +1,11 @@
+function Contact() {
+    return (
+        <div>
+            <h2>Contact Us</h2>
+            <p>Name: Williams</p>
+            <p>Phone: 9876543215</p>
+        </div>
+    );
+}
+
+export default Contact;
---
+++ seceFrontend/src/assets/components/classComponents/Functional Components/user.jsx
@@ -0,0 +1,9 @@
+function User({ email,password}){
+    return (
+        <div>
+            <h2>User Info</h2>
+            <p>Email: {email}</p>
+            <p>Password: {password}</p>
+        </div>
+    );
+}
