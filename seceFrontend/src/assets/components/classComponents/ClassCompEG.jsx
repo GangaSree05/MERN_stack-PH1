335d03fe85144439cc74da94a5f3046a10a7ee26
GangaSree <gangasree05@gmail.com>
2025-01-20 12:54:55 +0530

    Adding React files

---
+++ seceFrontend/src/assets/components/classComponents/ClassCompEG.jsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+class ClassCompEG extends React.Component{
+    render(){
+        return(
+            <div> This is a class Component Example </div>
+        )
+    }
+}
+export default ClassCompEG;
--- seceFrontend/src/assets/components/classComponents/Functional Components/home.jsx
+++ seceFrontend/src/assets/components/classComponents/Functional Components/home.jsx
@@ -1,4 +1,5 @@
-Home=()=>{
+import React from "react";
+const Home=()=>{
     return(
         <div>
         <h1>Welcome to react</h1>
