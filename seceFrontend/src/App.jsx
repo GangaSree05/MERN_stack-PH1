5501ab5a179acb5649cf90a7c19a3c3c75eacd31
GangaSree <gangasree05@gmail.com>
2025-01-20 16:00:13 +0530

    Adding files

--- seceFrontend/src/App.jsx
+++ seceFrontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 
-
+import './App.css'
 import About from './assets/components/classComponents/Functional Components/About'
 import ClassCompEG from './assets/components/classComponents/ClassCompEG'
 import Home from './assets/components/classComponents/Functional Components/home'
+import Gallery from './assets/components/classComponents/Functional Components/Gallery'
+//import Contact from './assets/components/classComponents/Functional Components/contact'
 
 function App() {
 
@@ -13,6 +15,11 @@ function App() {
         <Home/>
       <About/>
       <ClassCompEG/>
+      <h2>Without props</h2>
+      <Gallery page="Gallery" image="SECE Logo"/>
+      <h2>With props</h2>
+      <Gallery page="Gallery" image="SECE Logo"/>
+     
       </div>
     </>
   )
--- seceFrontend/src/assets/components/classComponents/Functional Components/contact.jsx
+++ seceFrontend/src/assets/components/classComponents/Functional Components/contact.jsx
@@ -1,12 +1,16 @@
-import React from "react";
-function Contact() {
+
+const Contact=()=> {
+    var [num,setNum]=useState(0);
     return (
         <div>
-            <h2>Contact Us</h2>
-            <p>Name: Williams</p>
-            <p>Phone: 9876543215</p>
+            <h2>Contact Us page</h2>
+            <h3> This is state example</h3>
+            <h4>Number is {num}</h4>
+            <button onClick={()=>setNum(num+1)}>+ </button>
         </div>
     );
-}
+};
 
 export default Contact;
+
+ 
